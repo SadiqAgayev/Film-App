04b5e805d67703ec1f80ede5ac599ae654668eb7
SadiqAgayev <sadiqagayev820@gmail.com>
2022-12-23 16:32:48 +0400

    rendering response from api

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,14 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Add = () => {
-    let [query,setQuery] = useState("")
-    function onChange(e) {
-        setQuery(e.target.value)
-    }
+  let [query, setQuery] = useState("");
+  let [data, setData] = useState([]);
+
+  useEffect(() => {
+   if (query.length) {
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=25fcdc88a9e8c6ac8725c01af2cc2530&language=pt-BR&query=${query}`
+    )
+      .then((resp) => resp.json())
+      .then((respData) => {
+        setData(respData.results)
+      });
+   }
+  },[query])
+
+  useEffect(() => console.log(data), [data])
+
+  function onChange(e) {
+    setQuery(e.target.value);
+    console.log(query)
+
+    
+  }
   return (
     <div className="add-page">
       <div className="container">
+        <div>
+          { data?.length ? data?.map((d) => {
+          return <img src={`http://image.tmdb.org/t/p/w500${d.poster_path}`} alt="" key={d.id} style={{width:"100px",height:"100px"}}/>;
+        }) : <p>loading</p>}
+        </div>
         <div className="add-content">
+          {}
           <img
             src="https://www.cinemaplus.az/site/assets/files/1083/6.jpg"
             style={{ height: "333px", opacity: 0.9 }}
@@ -22,7 +47,12 @@ const Add = () => {
             </h2>
           </div>
           <div className="input-wrapper">
-            <input type="text" onChange={onChange} placeholder="Search Film..." />
+            <input
+              type="text"
+              onChange={onChange}
+              placeholder="Search Film..."
+              value={query}
+            />
           </div>
         </div>
       </div>
